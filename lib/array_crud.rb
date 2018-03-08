a777f0b093d24343ad50ade2969b2d070043bdb1
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-08 13:33:09 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,23 +7,23 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("arrays!")
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift("wow")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop("arrays!")
+  array.pop(element)
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift("wow")
+  array.shift(element)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.slice!(array[2])
+  array.slice!(element[2])
 end
 
 def retrieve_first_element_from_array(array)
